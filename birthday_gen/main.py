1d98dd82695dbc52feddf9b3ea542ca8343e45b5
AbdelilahJaarani <167001400+AbdelilahJaarani@users.noreply.github.com>
2024-04-15 15:27:34 +0000

    Doube it

--- birthday_gen/main.py
+++ birthday_gen/main.py
@@ -15,6 +15,8 @@ today_tuple = (today.month, today.day)
 # HINT 2: Use pandas to read the birthdays.csv
 
 data = pandas.read_csv("birthdays.csv")
+data = pandas.read_csv("birthdays.csv")
+
 
 # HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
 # birthdays_dict = {
