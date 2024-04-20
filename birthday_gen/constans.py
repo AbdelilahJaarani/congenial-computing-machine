729058298ede612222fec2cfa262d89a24af13d2
AbdelilahJaarani <abdelilahjaarani@gmail.com>
2024-04-20 09:34:36 +0200

    modify the Script for working now

---
+++ birthday_gen/constans.py
@@ -0,0 +1,19 @@
+'''
+created by @AJ
+Constans for using the Automate Email Programm
+
+'''
+class constans:
+    def __init__(self) -> None:
+        self.email = "abdelilahjaarani@gmail.com"
+        self.password = "rtxckonfkdfoufdi"
+
+    def mail (self):
+        return self.email
+    
+
+
+    def passwort (self):
+        return self.password
+        
+    
